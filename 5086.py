651879c5dd88a4ce33bf5e4b440a50d75f157589
ddudu270 <strdf15@naver.com>
2020-04-03 16:07:54 +0900

    0403 new

---
+++ 5086.py
@@ -0,0 +1,13 @@
+import sys
+
+while True:
+    x, y = map(int, sys.stdin.readline().rstrip().split())
+    if x ==0 and y ==0:
+        break
+    else:
+        if x % y == 0:
+            print("multiple")
+        elif y % x == 0:
+            print("factor")
+        else:
+            print("neither")
