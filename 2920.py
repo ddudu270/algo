7e9491a3c7bd01c4cf01b7519894ab2244720ae9
ddudu270 <strdf15@naver.com>
2020-05-21 19:24:28 +0900

    0521 2920번 음계

---
+++ 2920.py
@@ -0,0 +1,10 @@
+import sys
+
+list = sys.stdin.readline().rstrip()
+
+if list == '1 2 3 4 5 6 7 8':
+    print('ascending')
+elif list == '8 7 6 5 4 3 2 1':
+    print('descending')
+else:
+    print('mixed')
