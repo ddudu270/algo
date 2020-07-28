f5e0ecd8ab0448fb215c8276e33532096cf9bb18
ddudu270 <strdf15@naver.com>
2020-07-28 23:32:16 +0900

    0728 화 실버1 번지수

--- BFS/2667.py
+++ BFS/2667.py
@@ -2,13 +2,22 @@ import sys
 from _collections import deque
 
 
-def bfs(i, j, count):
-    visit[i][j] = count # 방문을 count로 해줘야 나중에 셈이 가능
-    q = [[i,j]] # 큐에 지금 좌표를 넣어줌
-    grid[i][j] = 0 # 1을 0으로 바꿔버림
-    while q: # 이제부터 4방향 탐색 시작
-        x,y = q.popleft()
-
+def bfs(i, j, cnt):
+    num = 1
+    visit[i][j] = cnt  # 방문을 count로 해줘야 나중에 셈이 가능 (4번)
+    q = deque()  # 큐에 지금 좌표를 넣어줌
+    q.append((i, j))
+    grid[i][j] = 0  # 1을 0으로 바꿔버림
+    while q:  # 이제부터 4방향 탐색 시작
+        x, y = q.popleft()
+        for k in range(4):  # 2번
+            nx, ny = x + dx[k], y + dy[k]
+            if 0 <= nx < n and 0 <= ny < n:  # 벗어났는지 확인
+                if grid[nx][ny] == 1 and visit[nx][ny] == 0:
+                    num += 1
+                    q.append((nx, ny))
+                    visit[nx][ny] = cnt
+    return num
 
 
 n = int(sys.stdin.readline())
@@ -17,9 +26,16 @@ visit = [[0] * n for _ in range(n)]  # 방문 확인!
 count = 0
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
+ans = []
 
-for i in range(n):  # 노션 1번
+for i in range(n):  # 문제 해결 1번
     for j in range(n):
         if grid[i][j] == 1 and visit[i][j] == 0:
-            count += 1 # 노션 3번
-            bfs(i, j, count) # 노션 5번
+            count += 1  # 3번
+            ans.append(bfs(i, j, count))  # 5번
+
+ans.sort()
+
+print(count)
+for i in ans:
+    print(i)
